76f166b813449d6f4967fd2e1a603ff1f92d36e0
jahidmomin <mominjahid313@gmail.com>
2022-01-20 17:26:35 +0530

    add another way to auth

--- springboot-blog-app/src/main/java/com/springboot/blog/app/jwt/configuration/WebSecurityConfiguration.java
+++ springboot-blog-app/src/main/java/com/springboot/blog/app/jwt/configuration/WebSecurityConfiguration.java
@@ -68,4 +68,83 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 		authenticationManagerBuilder.userDetailsService(jwtService)
 		.passwordEncoder(encoder());
 	}
+	
+	
+	
+	
+/**	
+	
+	package com.mg.securityconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	JwtAuthenticationFilter jwtAuthenticationFilter;
+
+	@Autowired
+	JwtAuthenticationEntryPoint unauthorizedHandler;
+
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+
+				.antMatchers("/api/v1/user/patient/accept/reschedule/**").hasRole("patient")
+				.antMatchers("/api/v1/deviceparam/get/all/deviceparameters/**").hasRole("patient")
+				.antMatchers("/api/v1/consultation/get/configurations/**").hasRole("patient")
+				.antMatchers("/api/v1/consultation/get/test/result/**").hasRole("patient")
+				.antMatchers("/api/v1/consultation/send/testdata/**").hasRole("patient")
+				.antMatchers("/api/v1/appointment/booking/list/appointment/**").hasRole("patient")
+				.antMatchers("/api/v1/appointment/payment/summary/**").hasRole("patient")
+				.antMatchers("/api/v1/appointment/slot/locking/**").hasRole("patient")
+
+				.antMatchers("/api/v1/audiologist/**").hasRole("audiologist")
+				.antMatchers("/api/v1/appointment/accept/**").hasRole("audiologist")
+
+				.antMatchers("/api/v1/jwt/login").permitAll().antMatchers("/api/v1/auth/**").permitAll()
+
+				.antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui", "/swagger-resources",
+						"/swagger-resources/configuration/security", "/swagger-ui.html", "/webjars/**")
+				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and()
+				.exceptionHandling().authenticationEntryPoint(this.unauthorizedHandler).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Bean
+	AuthenticationManager authentication() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+}**/
+
 }
